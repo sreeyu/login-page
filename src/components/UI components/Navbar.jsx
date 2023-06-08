0ec01c7660a7b086910d1c28bc4c65e9ac77faa9
sreey <sreelakshmi12898@gmail.com>
2023-06-08 19:30:58 +0530

    Create Navbar component

---
+++ src/components/UI components/Navbar.jsx
@@ -0,0 +1,19 @@
+import styles from './Navbar.module.css'
+
+function Navbar(){
+    <nav className={styles.navbar}>
+        <ul>
+            <li>
+                <a href="/">Users</a>
+            </li>
+            <li>
+                <a href="/">Admin</a>
+            </li>
+            <li>
+                <button>Logout</button>
+            </li>
+        </ul>
+    </nav>
+};
+
+export default Navbar;
