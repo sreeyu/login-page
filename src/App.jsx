b3ac0a568b5bdaf607edb099b52be1d65d11ff20
sreey <sreelakshmi12898@gmail.com>
2023-06-09 20:25:58 +0530

    Let user stay logged in until the logout button is pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.css'
 import Login from './components/login/Login';
 import Home from './components/home/Home';
@@ -7,11 +7,20 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const userLoggedIn = localStorage.getItem('loggedIn');
+    if(userLoggedIn === '1'){
+      setIsLoggedIn(true);
+    }
+  },[])
+
   const getLogin = (email, password) => {
+    localStorage.setItem('loggedIn', '1');
     setIsLoggedIn(true);
   }
 
   const getLogOut = () => {
+    localStorage.removeItem('loggedIn')
     setIsLoggedIn(false);
   }
   
